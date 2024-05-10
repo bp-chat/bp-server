7418e0b32bfa7f819512f6cf9dc160e88158163f
Michel Barbosa <michel.amb.92@gmail.com>
2024-05-09 22:01:12 -0300

    Added test-client to the build

--- build.zig
+++ build.zig
@@ -15,9 +15,15 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const main_exe = b.addExecutable(.{
         .name = "bp-server",
         .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const test_client_exe = b.addExecutable(.{
+        .name = "bp-test-client",
+        .root_source_file = b.path("src/test-client.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -25,42 +31,55 @@ pub fn build(b: *std.Build) void {
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
-    b.installArtifact(exe);
+    b.installArtifact(main_exe);
+    b.installArtifact(test_client_exe);
 
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+    const run_main = b.addRunArtifact(main_exe);
+    const run_test_client = b.addRunArtifact(test_client_exe);
 
     // By making the run step depend on the install step, it will be run from the
     // installation directory rather than directly from within the cache directory.
     // This is not necessary, however, if the application depends on other installed
     // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+    run_main.step.dependOn(b.getInstallStep());
+    run_test_client.step.dependOn(b.getInstallStep());
 
     // This allows the user to pass arguments to the application in the build
     // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
-        run_cmd.addArgs(args);
+        run_main.addArgs(args);
+        run_test_client.addArgs(args);
     }
 
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const run_main_step = b.step("run", "Run the server");
+    run_main_step.dependOn(&run_main.step);
+    const run_test_client_step = b.step("run-client", "Run test client");
+    run_test_client_step.dependOn(&run_test_client.step);
 
-    const exe_unit_tests = b.addTest(.{
+    const exe_main_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    const exe_test_client_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/test-client.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_main_unit_tests = b.addRunArtifact(exe_main_unit_tests);
+    const run_test_client_unit_tests = b.addRunArtifact(exe_test_client_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_main_unit_tests.step);
+    test_step.dependOn(&run_test_client_unit_tests.step);
 }
--- src/TODO.md
+++ src/TODO.md
@@ -1,7 +1,9 @@
 # TODO
 
-- [ ] Remove root.zig from build
-- [ ] Add test-client to the build
 - [ ] Pass listening port to the server via args
 - [ ] Pass address to the client via args
 
+# DONE
+
+- [x] Add test-client to the build
+- [x] Remove root.zig from build
