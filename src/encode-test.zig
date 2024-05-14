27bf3c98a8a8fda70d3ee2c0782309c2f92b414d
Pedro Santos <phpdro@gmail.com>
2024-05-13 22:11:35 -0300

    wip: add some decode logic

--- src/encode-test.zig
+++ src/encode-test.zig
@@ -5,19 +5,39 @@ const test_allocator = std.testing.allocator;
 
 const FictionalCommand = struct { version: u16, command_ref_id: u8, command_id: u16 };
 
-//TUDO add \n chars and write the rest of the u16 data
-fn encode(command: FictionalCommand) std.mem.Allocator.Error![]u8 {
-    var res = std.ArrayList(u8).init(test_allocator);
-    defer res.deinit();
-    try res.append(@truncate(command.version));
-    try res.append(command.command_ref_id);
-    try res.append(@truncate(command.command_id));
-    return res.items;
+fn encode(command: FictionalCommand) []u8 {
+    var res: [7]u8 = undefined;
+    res[0] = @truncate(command.version >> 8);
+    res[1] = @truncate(command.version);
+    res[2] = 0x0A;
+    res[3] = command.command_ref_id;
+    res[4] = 0x0A;
+    res[5] = @truncate(command.command_id >> 8);
+    res[6] = @truncate(command.command_id);
+    return &res;
 }
 
 test "should encode" {
-    const cmd = FictionalCommand{ .version = 1, .command_ref_id = 2, .command_id = 3 };
-    const actual = try encode(cmd);
-    //TUDO test the actual written bytes
-    try testing.expect(actual.len == 3);
+    const cmd = FictionalCommand{ .version = 0xF00F, .command_ref_id = 2, .command_id = 0x0FF0 };
+    const actual = encode(cmd);
+    try testing.expect(actual.len == 7);
+    try testing.expect(actual[0] == 0xF0);
+    try testing.expect(actual[1] == 0x0F);
+    try testing.expect(actual[2] == 0x0A);
+    try testing.expect(actual[3] == 2);
+    try testing.expect(actual[4] == 0x0A);
+    try testing.expect(actual[5] == 0x0F);
+    try testing.expect(actual[6] == 0xF0);
+}
+
+fn decode(data: []u8) FictionalCommand {
+    return FictionalCommand{ .version = (@as(u16, data[0]) << 8) | @as(u16, data[1]), .command_ref_id = data[3], .command_id = (@as(u16, data[5]) << 8) | @as(u16, data[6]) };
+}
+
+test "should decode" {
+    var input = [7]u8{ 0xF0, 0x0F, 0x0a, 0x03, 0x0a, 0x1f, 0xf1 };
+    const actual = decode(&input);
+    try testing.expect(actual.version == 0xf00f);
+    try testing.expect(actual.command_ref_id == 3);
+    try testing.expect(actual.command_id == 0x1ff1);
 }
