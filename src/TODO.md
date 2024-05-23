d6fd83943c7ff2d264feeee6032241f495971d6e
Michel Barbosa <michel.amb.92@gmail.com>
2024-05-23 09:53:43 -0300

    Passing port parameter to client.

--- src/TODO.md
+++ src/TODO.md
@@ -1,11 +1,11 @@
 # TODO
 
-- [ ] Pass address to the client via args
 - [ ] Read dummy message
 - [ ] Relay dummy message
 
 # DONE
 
+- [x] Pass address to the client via args
 - [x] Pass listening port to the server via args
 - [x] Add test-client to the build
 - [x] Remove root.zig from build
--- src/test-client.zig
+++ src/test-client.zig
@@ -2,10 +2,31 @@ const std = @import("std");
 const net = std.net;
 
 pub fn main() !void {
-    // 'B' = 66, 'P' = 80
-    const default_port: u16 = 6680;
-    const address = try net.Address.parseIp("127.0.0.1", default_port);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+    _ = args.next();
+    const port = try getPort(&args);
+
+    const address = try net.Address.parseIp("127.0.0.1", port);
+    std.log.info("Attempting to connect in {}", .{address});
     const stream = try net.tcpConnectToAddress(address);
     defer stream.close();
     std.log.info("Connected!", .{});
+}
+
+fn getPort(args: *std.process.ArgIterator) !u16 {
+    const param_name_candidate = args.next();
+    if (param_name_candidate != null and (std.mem.eql(u8, param_name_candidate.?, "-p") or std.mem.eql(u8, param_name_candidate.?, "--port"))) {
+        const port_candidate = args.next();
+        if (port_candidate != null) {
+            return try std.fmt.parseInt(u16, port_candidate.?, 10);
+        }
+    }
+    // 'B' = 66, 'P' = 80
+    const default_port: u16 = 6680;
+    std.log.info("No custom port provided, using default: {}", .{default_port});
+    return default_port;
 }
