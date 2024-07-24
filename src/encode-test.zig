d33bc1c5faf4c5a7884bf770b5b71643bcf9713f
Pedro Santos <phpdro@gmail.com>
2024-07-23 22:37:37 -0300

    chore: Add an encode method to the command struct

--- src/encode-test.zig
+++ src/encode-test.zig
@@ -3,24 +3,53 @@ const testing = std.testing;
 const assert = std.debug.assert;
 const Endian = std.builtin.Endian;
 
+const CURRENT_VERSION = 1;
+
 const Header = struct {
     version: u16,
-    command_ref_id: u8,
-    command_id: u16,
+    commandRefId: u8,
+    commandId: u16,
 };
 const Command = struct {
+    const maxLength = 4096;
+
     header: Header,
-    body: []u8,
+    body: [4091]u8,
+
+    fn encode(self: Command, buffer: *[Command.maxLength]u8) void {
+        std.mem.writeInt(u16, buffer[0..2], self.header.version, Endian.big);
+        buffer[2] = self.header.commandRefId;
+        std.mem.writeInt(u16, buffer[3..5], self.header.commandId, Endian.big);
+        buffer[5..].* = self.body;
+    }
 };
 
 const MessageCommand = struct {
+    const length = 1040;
+    const commandId = 1;
+
+    //TODO replace fields for methods
+    //TODO test if buffer could be a pointer
     recipient: [16]u8,
     message: [1024]u8,
+    buffer: [length]u8,
 
-    fn parse(data: [1040]u8) MessageCommand {
+    fn parse(data: [length]u8) MessageCommand {
         return MessageCommand{
+            .buffer = data,
             .recipient = data[0..16],
             .message = data[16..1040],
+        };
+    }
+
+    fn asCommand(self: MessageCommand) Command {
+        return Command{
+            .header = Header{
+                .version = CURRENT_VERSION,
+                .commandRefId = 0,
+                .commandId = commandId,
+            },
+            .body = self.buffer,
         };
     }
 };
@@ -29,19 +58,23 @@ fn decode_header(data: []u8) Header {
     assert(data.len >= 5);
     return Header{
         .version = std.mem.readInt(u16, data[0..2], Endian.big),
-        .command_ref_id = data[2],
-        .command_id = std.mem.readInt(u16, data[3..5], Endian.big),
+        .commandRefId = data[2],
+        .commandId = std.mem.readInt(u16, data[3..5], Endian.big),
     };
 }
-
+//TODO consider moving to Command struct
+//TODO consider accepting a fixed length array/slice
 fn decode(data: []u8) Command {
     assert(data.len >= 5);
-    //TODO ASSERT MAX LEN
+    assert(data.len <= Command.maxLength);
     // For some reason the asBytes and bytesAsValues funtions does not layout
     // the bytes of the struct in the way that I expected
     // field by field in the order that was declared, maybe we should rethink the struct
     // because the llvm is optmizing the struct?
-    return Command{ .header = decode_header(data[0..5]), .body = data[5..] };
+    return Command{
+        .header = decode_header(data[0..5]),
+        .body = data[5..],
+    };
 }
 
 test "should decode header" {
@@ -60,6 +93,31 @@ test "should decode header" {
     const actual = decode_header(&input);
     std.debug.print("\ndecoded {}\n", .{actual});
     try testing.expectEqual(0x01, actual.version);
-    try testing.expectEqual(0x02, actual.command_ref_id);
-    try testing.expectEqual(0x03, actual.command_id);
+    try testing.expectEqual(0x02, actual.commandRefId);
+    try testing.expectEqual(0x03, actual.commandId);
+}
+
+test "should enconde command" {
+    var buffer: [Command.maxLength]u8 = undefined;
+    buffer[0] = 0;
+    var cmdBody: [4091]u8 = undefined;
+    cmdBody[0] = 4;
+    cmdBody[1] = 5;
+    cmdBody[2] = 6;
+    cmdBody[3] = 7;
+    cmdBody[4] = 8;
+    cmdBody[5] = 9;
+    const cmd = Command{
+        .header = Header{
+            .version = 1,
+            .commandRefId = 2,
+            .commandId = 3,
+        },
+        .body = cmdBody,
+    };
+    cmd.encode(&buffer);
+    try testing.expectEqual(0, buffer[0]);
+    try testing.expectEqual(1, buffer[1]);
+    try testing.expectEqual(2, buffer[2]);
+    try testing.expectEqual(8, buffer[9]);
 }
